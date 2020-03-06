150f182214c2376028fec8f3c353e3a18dfcd33a
Eric Douglas <eric.douglas2036@gmail.com>
2020-03-06 11:47:46 -0700

    Finalized version

--- Dice/dice/src/App.js
+++ Dice/dice/src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Body>
-
-        </Body>
+        <Body></Body>
       </header>
     </div>
   );
--- Dice/dice/src/dice.js
+++ Dice/dice/src/dice.js
@@ -2,45 +2,46 @@
 import React, { Component } from 'react'
 
 export default class Body extends Component {
+  state = {
+    dieSides: [6, 12, 20]
+  } // we use an array so each number doesn't have to be set elsewhere. It's easier to target a collection
+
+  render() {
+    return this.state.dieSides.map(die => {
+      return <Number key={die} sides={die} />
+    })
+  }
+
+}
+
+class Number extends Component {
   constructor(props) {
     super(props)
-    
     this.state = {
       number: 0
     };
     this.generateRandomNumber = this.generateRandomNumber.bind(this);
-  } // Class is made here. I choose body for simplicity's sake even though I don't know if it's a member or React or not
 
-  generateRandomNumber = () => {
+  }
+
+  generateRandomNumber = (sides) => {
     const min = 1
-    const max = 13
-    const actualNum = Math.floor(min + Math.random() * (max - min))
-    console.log("generated")
-    this.setState({number: actualNum})
-  } // Here's a function. I stole it from the web and have made some modification. It seemed malleable enough for my taste.
-  // Math.floor rounds it so no decimals occur.
+    const max = this.props.sides + 1
+    const actualNum = parseInt(Math.floor(min + Math.random() * (max - min)))
+    console.log("generated", actualNum)
 
-  // LEAVE ALL THIS ALONE BELOW
-  render(){
-    return(
+    this.setState({ number: actualNum })
+  }
+
+  render() {
+    return (
       <div>
-        <button onClick={this.generateRandomNumber}>Random number</button>
-        <Number myNumber={this.state.number}/>
+        <button onClick={(event, sides = 6) => this.generateRandomNumber(event)}>Sides {this.props.sides}
+        </button>
+        <p>{this.state.number}</p>
       </div>
     );
   }
-} 
+}
 
-class Number extends Component {
-  render(){
-    return(
-      <div>
-        <br />
-        <p>{this.props.myNumber}</p>
-      </div>
-    );
-  }
-} 
-
-//for more promising results, use the code above 
 
